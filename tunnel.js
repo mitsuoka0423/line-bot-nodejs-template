0ab0f1fe4c1dad90dd513a390a9259685be9022a
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-12-24 20:47:31 +0900

    feat: automated to update LINE webhook endpoint URL

--- tunnel.js
+++ tunnel.js
@@ -1,7 +1,26 @@
+require('dotenv').config();
 const ngrok = require('ngrok');
 
 const port = process.env.PORT || 13000;
 
-ngrok.connect(port).then((url) => {
-  console.log(url);
-});
+const main = async () => {
+  const url = await ngrok.connect(port);
+  console.log(`Tunneling local ${port} port to ${url}`);
+
+  const response = await fetch('https://api.line.me/v2/bot/channel/webhook/endpoint', {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ endpoint: url }),
+  });
+  if (response.status >= 400) {
+    const e = await response.json();
+    throw new Error(e.message);
+  }
+
+  console.log('LINE webhook endpoint is updated.');
+};
+
+main();
